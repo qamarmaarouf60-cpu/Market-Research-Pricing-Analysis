f486d48813389cd0a30407a1dbe99bab30975622
QamarMaarouf <qamarmaarouf60@gmail.com>
2026-04-28 19:27:37 +0100

    fix jumia_parser.py et runner.py

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -2,10 +2,8 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.FloatField()
-    source = models.CharField(max_length=100)  # Avito, Jumia...
+    price_text = models.CharField(max_length=50)   # "7,975.00 Dhs"
+    price_value = models.FloatField()               # 7975.0
+    source = models.CharField(max_length=100)
     url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.name
--- backend/scraping/runner.py
+++ backend/scraping/runner.py
@@ -1,7 +1,38 @@
+import os
+import django
+import re
+
+# 1. IMPORTANT : activer Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
+
+# 2. imports Django après setup
+from apps.products.models import Product
 from scraping.spiders.jumia import fetch_jumia_html
 from scraping.parsers.jumia_parser import parse_jumia
 
-html = fetch_jumia_html()
-data = parse_jumia(html)
 
-print(data)
+# 3. fonction nettoyage prix
+def extract_price(price_str):
+    numbers = re.findall(r"[\d,.]+", price_str)
+    if numbers:
+        return float(numbers[0].replace(",", ""))
+    return 0.0
+
+
+# 4. pipeline principal
+html = fetch_jumia_html()
+products = parse_jumia(html)
+
+print(f"{len(products)} produits trouvés")
+
+for item in products:
+    Product.objects.create(
+        name=item["name"],
+        price_text=item["price"],
+        price_value=extract_price(item["price"]),
+        source=item["source"],
+        url=item["url"]
+    )
+
+print("Données enregistrées en base avec succès")
