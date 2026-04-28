a1e8969f7130da213caff02dfa6e951c1da202e4
QamarMaarouf <qamarmaarouf60@gmail.com>
2026-04-28 22:19:47 +0100

    clean repo + add gitignore

--- backend/apps/products/serializers.py
+++ backend/apps/products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Product
 
 
+
 class ProductSerializer(serializers.ModelSerializer):
 
     # ── Champ calculé : catégorie de prix ─────────────────────────────────────
