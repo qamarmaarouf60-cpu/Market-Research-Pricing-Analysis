2684c2603e6a6cd36fb8a10498eff6fd1749ccaf
QamarMaarouf <qamarmaarouf60@gmail.com>
2026-04-29 19:20:14 +0100

    implemente backend/apps/analytics

--- backend/apps/analytics/models.py
+++ backend/apps/analytics/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class PriceSnapshot(models.Model):
+    category = models.CharField(max_length=100)
+    avg_price = models.FloatField()
+    min_price = models.FloatField()
+    max_price = models.FloatField()
+    count = models.IntegerField()
+    calculated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.category} - {self.calculated_at}"
