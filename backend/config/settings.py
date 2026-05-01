454e152d16065ac2b7f476a8af61ca2ce8b498c1
QamarMaarouf <qamarmaarouf60@gmail.com>
2026-05-01 16:52:52 +0100

    fix config/settings.py en ajoutant django-cors-headers qui sert à autoriser le frontend (React) à communiquer avec le backend (Django) en levant les blocages de sécurité imposés par les navigateurs

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,14 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'rest_framework',
 
     'apps.products',
     'apps.search',
     'apps.analytics',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CORS_ALLOW_ALL_ORIGINS = True
